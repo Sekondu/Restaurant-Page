d412c689b1cb5ccd16b28dd2f24e31abcbf5de14
musashi <rwid2005@gmail.com>
2025-05-26 16:41:54 +0300

    "some changes to ensure functionality"

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path=require("path");
 const HtmlWebpackPlugin=require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports={
     mode:"development",
@@ -12,13 +13,16 @@ module.exports={
     plugins:[
         new HtmlWebpackPlugin({
             template:"./src/index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
     ],
     module:{
         rules:[
             {
                 test:/\.css$/i,
-                use:["style-loader","css-loader"],
+                use:[MiniCssExtractPlugin.loader, "css-loader"],
             },
             {
                 test:/\.html$/i,
